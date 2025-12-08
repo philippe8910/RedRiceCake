fb8bc9b9eec9172f09eb6adecd765599c41d699d
philippe cheng <philippe8910@gmail.com>
2025-12-08 19:01:55 +0800

    99

---
+++ Assets/SM_Loader.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SM_Loader : MonoBehaviour
+{
+    public void loader()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
